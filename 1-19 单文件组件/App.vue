933e2680bd1ff603540c6d5e42acf64ea568f244
wency <384194598@qq.com>
2021-10-17 16:35:06 +0800

    master

---
+++ 1-19 单文件组件/App.vue
@@ -0,0 +1,21 @@
+/* 功能:汇总所有的组件 */
+<template>
+    <div>
+        <School/>   <!-- 脚手架环境下不报错 -->
+        <Student/>
+    </div>
+</template>
+
+<script>
+    //引入组件
+    import School from './School.vue'
+    import Student from './Student.vue'
+
+    export default {
+        name:'App',
+        components:{
+            School,
+            Student
+        }
+    };
+</script>
---
+++ 1-19 单文件组件/main.js
@@ -0,0 +1,6 @@
+import App from "./App.vue";
+new Vue({
+    el:'#root',
+    template:`<App></App>`,
+    components:{App}
+})
